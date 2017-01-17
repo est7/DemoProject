e816af439775185206160ebbef66d8b6cd8a0b22
est7 <est7@qq.com>
2017-01-17 16:02:40 +0800

    终于成功了

--- app/src/main/java/com/est7/demoproject/ListViewPager/ListViewPagerActivity.java
+++ app/src/main/java/com/est7/demoproject/ListViewPager/ListViewPagerActivity.java
@@ -3,9 +3,8 @@ package com.est7.demoproject.ListViewPager;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
-import com.est7.demoproject.ListViewPager.diyviewpager.LisViewPagerAdapter;
-import com.est7.demoproject.ListViewPager.diyviewpager.ListViewPager;
 import com.est7.demoproject.R;
 
 import java.util.ArrayList;
@@ -13,11 +12,13 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ListViewPagerActivity extends AppCompatActivity {
 
-    @BindView(R.id.vp_list)
-    ListViewPager mVpList;
+    @BindView(R.id.btn_1)
+    Button mBtn1;
+    private List mList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,53 +31,50 @@ public class ListViewPagerActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        List list = new ArrayList<Drawable>();
+        mList = new ArrayList<Drawable>();
 
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
-        list.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
+        mList.add(this.getResources().getDrawable(R.mipmap.ic_launcher));
 
-        mVpList.setAdapter(new LisViewPagerAdapter(this,list, 7));
+        //mVpList.setAdapter(new LisViewPagerAdapter(this, mList, 7));
+
+
+    }
+
+    @OnClick(R.id.btn_1)
+    public void onClick() {
+
+
+        ShowerCard showerCard = new ShowerCard(this);
+        showerCard.showDialog("", "", "");
+
 
     }
 }
---
+++ app/src/main/java/com/est7/demoproject/ListViewPager/ShowerCard.java
@@ -0,0 +1,110 @@
+package com.est7.demoproject.ListViewPager;
+
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.est7.demoproject.ListViewPager.diyviewpager.LisViewPagerAdapter;
+import com.est7.demoproject.ListViewPager.diyviewpager.ListViewPager;
+import com.est7.demoproject.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/1/16.
+ */
+
+public class ShowerCard {
+
+    private ShowerDialog mShowerDialog;
+    private Context mContext;
+    private ListViewPager mRcBadgeList;
+
+    public ShowerCard(Context context) {
+        mShowerDialog = new ShowerCard.ShowerDialog(context);
+        mContext = context;
+        init();
+
+    }
+
+    private void requestHttp(String vsId, String showerId, String actionVsId) {
+
+
+    }
+
+    private void init() {
+        mShowerDialog.setCancelable(true);
+        mShowerDialog.setCanceledOnTouchOutside(true);
+        mShowerDialog.setContentView(R.layout.dialog_shower_card);
+        Window window = mShowerDialog.getWindow();
+        WindowManager.LayoutParams lp = window.getAttributes();
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.gravity = Gravity.BOTTOM;
+        window.setAttributes(lp);
+
+        mRcBadgeList = (ListViewPager) mShowerDialog.findViewById(R.id.list_view_pager);
+
+
+        List list = new ArrayList<Drawable>();
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+        list.add(mContext.getResources().getDrawable(R.mipmap.ic_launcher));
+
+        mRcBadgeList.setAdapter(new LisViewPagerAdapter(mContext,list, 7 ));
+    }
+
+    public void showDialog(String vsId, String showerId, String actionVsId) {
+
+        requestHttp(vsId, showerId, actionVsId);
+
+        if (!mShowerDialog.isShowing()) {
+            mShowerDialog.show();
+        }
+
+
+    }
+
+
+    public void dismissDialog() {
+        if (mShowerDialog.isShowing()) {
+            mShowerDialog.dismiss();
+        }
+
+
+    }
+
+    private class ShowerDialog extends Dialog {
+        public ShowerDialog(Context context) {
+            super(context, R.style.BottomDialog);
+        }
+    }
+
+
+}
